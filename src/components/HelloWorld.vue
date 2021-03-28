78f51271ae0f61b527adf7781854497424994f8f
bisbis233 <75820216+bisbis233@users.noreply.github.com>
2021-03-28 22:26:23 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,6 @@
+
 <template>
+
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
@@ -28,15 +30,22 @@
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
+
 </template>
 
+
+
 <script>
+
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   }
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
